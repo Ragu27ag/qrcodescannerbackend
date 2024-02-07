ffda36e66aeeb9f4a181f4338d6dc926fc9859a2
Ragunath <raguyuvi27@gmail.com>
2024-02-07 18:45:59 +0530

    ts to js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-"use strict";
-// Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var cors_1 = require("cors");
-var app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use((0, cors_1.default)());
-app.listen("3000", function () {
-  return console.log("server is running");
-});
+import express from "express";
+import cors from "cors";
+import { dataRouter } from "./Router/datarouter";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+app.get("/health", (req, res) => res.send("up and running"));
+
+app.use("/scannedData", dataRouter);
+
+mongoose
+  .connect("mongodb://localhost:27017/qrcode")
+  .then(() =>
+    app.listen("5000", () =>
+      console.log("server is running && db is connected")
+    )
+  )
+  .catch((e) => console.log(e));
